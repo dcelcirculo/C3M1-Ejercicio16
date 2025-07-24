baf5eff3ccb9b88928a4b26dcad4eff96a0a1b31
CaroGil <dcgil83@gmail.com>
2025-07-23 18:58:53 -0700

    First commit

---
+++ src/App.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int number = pedirDatos(entrada, "Ingrese un número: ");
+        int counter = 0;
+        while (number < 0) {
+            number = pedirDatos(entrada, "Ingrese un número: ");
+            counter++;
+        }
+        System.out.println(counter);
+
+    }
+
+    public static int pedirDatos(Scanner entrada, String mensaje) {
+
+        int number = 0;
+        boolean validator = false;
+        do {
+            System.out.print(mensaje);
+            if (entrada.hasNextInt()) {
+                number = entrada.nextInt();
+                validator = true;
+            } else {
+                System.out.println("Entrada inválida. Intente nuevamente.");
+                entrada.next();
+            }
+        } while (!validator);
+
+        return number;
+    }
+}
